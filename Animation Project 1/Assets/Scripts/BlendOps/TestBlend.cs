5f088a0411fd79c654da5f3b8676e613268d9019
mz <michael.zheng@mymail.champlain.edu>
2019-10-22 12:17:51 -0400

    blending operations

---
+++ Animation Project 1/Assets/Scripts/BlendOps/TestBlend.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBlend : MonoBehaviour
+{
+    protected Transform poseresult;
+    public bool usingQuaternion = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poseresult = this.gameObject.transform;
+    }
+
+}
---
+++ Animation Project 1/Assets/Scripts/BlendOps/testBlendScale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testBlendScale : TestBlend
+{
+    public Transform pose_1 = null;
+
+    [Range(0.0f, 1.0f)]
+    public float parameter = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Logic:same as lerp operation as if pose_0 is pose_identity
+    }
+}
