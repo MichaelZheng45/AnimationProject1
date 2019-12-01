1cbe322b10fde46da08d15378801e44e8b9b3aa5
Michael Zheng <michael.zheng@mymail.champlain.edu>
2019-12-01 01:29:57 -0500

    started animation hierarchy window

---
+++ Animation Project 1/Assets/Scripts/WindowsEditor/AnimationHierarchyEditor.cs
@@ -0,0 +1,28 @@
+﻿  using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AnimationHierarchyEditor : EditorWindow
+{
+	public AnimationDataHierarchal animData;
+	public gameObjectMain gameObjectHierarchy;
+	int currentKeyFrame;
+
+	[MenuItem("Window/AnimationHierarchyEditor")]
+	public static void ShowWindow()
+	{
+		EditorWindow.GetWindow(typeof(AnimationHierarchyEditor));
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnGUI()
+	{
+		
+	}
+}
